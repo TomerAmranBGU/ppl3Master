9ba490917a7ce41ae9ae83baad5a8635db6b2887
Or Saar <orsaar0@gmail.com>
2020-05-19 13:33:50 +0300

    predicates for type unions

--- part2/mermaid-ast.ts
+++ part2/mermaid-ast.ts
@@ -11,9 +11,11 @@
 // <nodeRef> ::= <identifier> // NodeRef(id: string)
 // <edgeLabel> ::= |<identifier>| // string
 
+// A value returned by parse
 export type GraphContent = AtomicGraph | CompoundGraph;
 export type Node = NodeDecl | NodeRef;
 export type Dir = TD | LR;
+
 export interface TD{tag:"TD"}
 export interface LR{tag:"LR"}
 export interface Graph {tag:"Graph"; dir: Dir ; content: GraphContent}
@@ -23,6 +25,8 @@ export interface Edge{tag:"Edge"; parent: Node ;  child: Node; lable?: EdgeLable
 export interface NodeDecl{tag:"NodeDecl"; id: string; lable: string}
 export interface NodeRef{tag:"NodeRef"; id:string}
 export interface EdgeLable{tag:"EdgeLable"; id:string}
+
+// Type value constructors for disjoint types
 export const makeTD = (): TD => ({tag:"TD"});
 export const makeLR = (): LR => ({tag:"LR"});
 export const makeGraph = (dir: Dir , content: GraphContent): Graph => ({tag:"Graph" , dir:dir , content:content});
@@ -33,8 +37,8 @@ export const makeNodeDecl = (id: string, lable: string): NodeDecl => ({tag:"Node
 export const makeNodeRef = (id:string): NodeRef => ({tag:"NodeRef", id:id});
 export const makeEdgeLable = (id:string): EdgeLable=> ({tag:"EdgeLable", id:id});
 
-
-export const isGraph = (x : any): x is Graph=> x.tag == "Graph";
+// Type predicates for disjoint types
+export const isGraph = (x: any): x is Graph => x.tag == "Graph";
 export const isTD = (x: any): x is TD => x.tag == "TD";
 export const isLR = (x: any): x is LR => x.tag == "LR";
 export const isAtomicGraph = (x: any): x is AtomicGraph => x.tag == "AtomicGraph";
@@ -43,3 +47,8 @@ export const isNodeDecl = (x: any): x is NodeDecl => x.tag == "NodeDecl";
 export const isNodeRef = (x: any): x is NodeRef => x.tag == "NodeRef";
 export const isEdge = (x: any): x is Edge => x.tag == "Edge";
 export const isEdgeLable = (x: any): x is EdgeLable => x.tag == "EdgeLable";
+
+// Type predicates for type unions
+export const isGraphContent = (x: any): x is GraphContent => isAtomicGraph(x) || isCompoundGraph(x);
+export const isNode = (x: any): x is Node => isNodeDecl(x) || isNodeRef(x);
+export const isDir = (x: any): x is Dir => isTD(x) || isLR(x);
